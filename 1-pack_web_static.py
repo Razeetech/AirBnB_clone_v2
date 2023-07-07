4f3315b1f9838fc0f58df517fdc4ac41f3c4bf47
Razeetech <rajiazeezsch@gmail.com>
2023-07-06 17:59:59 -0700

    0x03. AirBnB clone - Deploy static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""module pack up in tar format"""
+
+import os
+import os.path
+import datetime
+import fabric.api
+
+
+def do_pack():
+    """compress + bundle local sweb files"""
+    try:
+        if os.path.isdir("versions") is False:
+                os.mkdir("versions")
+        time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        packed = 'versions/web_static_' + time + '.tgz'
+        fabric.api.local("tar -cvzf {} web_static".format(packed))
+        return packed
+    except:
+        return None
